43439a480a9c17fa29307b244f7f9e2e00b9651b
Tomasz Holowka <tomaszholowka@gmail.com>
2014-11-26 17:40:26 +0000

    Results work

--- events/results/results_test.go
+++ events/results/results_test.go
@@ -3,26 +3,33 @@ package results
 import (
     "testing"
     "github.com/tholowka/testing/assertions"
+    "log"
 )
 
 func TestThat_First_ThrowsErrors(t *testing.T) {
     //given
     returnsAnError(New(aString).First, t)
-
+    doesNotReturnAnError(New(aStringArray).First, t)
 }
 
 func returnsAnError(method func() (interface{}, error), t *testing.T) {
     result, err := method()
+    log.Println(err)
     assertions.New(t).IsNil(result).IsNotNil(err)
 }
 
 func doesNotReturnAnError(method func() (interface{}, error), t *testing.T) {
     result, err := method()
+    log.Println(err)
     assertions.New(t).IsNotNil(result).IsNil(err)
 }
 
 func aString() interface{} {
     return "mingus"
+}
+
+func aStringArray() interface{} {
+    return []string { "mingus", "ella" }
 }
 
 func anInt() interface{} {
