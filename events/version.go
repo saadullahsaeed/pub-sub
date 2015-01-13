fd954d9dd0a86ead25cc9b1a1dc2ae20d40d86f7
Tomasz Holowka <t.holowka@recordsure.com>
2015-01-13 15:06:11 +0000

    Catching nil Topics passed in And, Or

--- events/version.go
+++ events/version.go
@@ -2,5 +2,5 @@ package events
 
 /* Returns the version of this library. The versioning method is compliant with Semantic Versioning principles. */
 func Version() string {
-    return "1.0"
+    return "1.1"
 }
