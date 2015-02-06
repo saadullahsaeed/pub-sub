f84d27d718e7e9461670bd41bd1c5c965e205cff
tomasz <tomaszholowka@gmail.com>
2015-02-06 18:27:36 +0000

    Defensive code around Close()
    People might call Close repeatedly.

--- events/simple_topic.go
+++ events/simple_topic.go
@@ -1,47 +1,48 @@
 package events
 
 type simpleTopic struct {
-    p *factory
-    name string
-    optionalState interface{}
+	p             *factory
+	name          string
+	optionalState interface{}
 }
 
 func (t *simpleTopic) String() string {
-    return t.name
+	return t.name
 }
 
 func (t *simpleTopic) NewPublisher() Publisher {
-    publisher := func(event interface{}) {
-        //it's crucial this is in a go-routine: running 2+ Publishers in the same
-        //go-routine causes a deadlock without this.
-        go func() {
-            t.p.events<- &eventSpec { t.name, event }
-        }()
-    }
-    return publisher
+	publisher := func(event interface{}) {
+		//it's crucial this is in a go-routine: running 2+ Publishers in the same
+		//go-routine causes a deadlock without this.
+		go func() {
+			t.p.events <- &eventSpec{t.name, event}
+		}()
+	}
+	return publisher
 }
 
 func (t *simpleTopic) NewSubscriber(subscriber Subscriber) {
-    stateChanged := make(chan bool)
-    adder := func(p *factory) {
-        if subscriber != nil {
-            p.subscribers[t.name] = append(p.subscribers[t.name], subscriber)
-        }
-    }
-    t.p.stateModifier <- &stateModifierSpec { adder, stateChanged, false }
-    <-stateChanged
-    close(stateChanged)
+	stateChanged := make(chan bool)
+	adder := func(p *factory) {
+		if subscriber != nil {
+			p.subscribers[t.name] = append(p.subscribers[t.name], subscriber)
+		}
+	}
+	t.p.stateModifier <- &stateModifierSpec{adder, stateChanged, false}
+	<-stateChanged
+	close(stateChanged)
 }
 
 func (t *simpleTopic) Close() error {
-    stateChanged := make(chan bool)
-    remover := func(state *factory) {
-        delete(state.topics, t.name)
-        delete(state.subscribers, t.name)
-    }
-    t.p.stateModifier <- &stateModifierSpec { remover, stateChanged, false }
-    <-stateChanged
-    close(stateChanged)
-    return nil
+	stateChanged := make(chan bool)
+	remover := func(state *factory) {
+		if _, exists := state.topics[t.name]; exists {
+			delete(state.topics, t.name)
+			delete(state.subscribers, t.name)
+		}
+	}
+	t.p.stateModifier <- &stateModifierSpec{remover, stateChanged, false}
+	<-stateChanged
+	close(stateChanged)
+	return nil
 }
-
--- events/ticker_topic.go
+++ events/ticker_topic.go
@@ -1,64 +1,66 @@
 package events
 
 import (
-    "time"
+	"time"
 )
+
 type tickerTopic struct {
-    p *factory
-    name string
-    ticker *time.Ticker
-    closeChannel chan bool
+	p            *factory
+	name         string
+	ticker       *time.Ticker
+	closeChannel chan bool
 }
 
 func (t *tickerTopic) String() string {
-    return t.name
+	return t.name
 }
 
 func (t *tickerTopic) NewPublisher() Publisher {
-    panic("Tickers can't be published to")
+	panic("Tickers can't be published to")
 }
 
 func (t *tickerTopic) NewSubscriber(subscriber Subscriber) {
-    stateChanged := make(chan bool)
-    adder := func(p *factory) {
-        if subscriber != nil {
-            p.subscribers[t.name] = append(p.subscribers[t.name], subscriber)
-        }
-    }
-    t.p.stateModifier <- &stateModifierSpec { adder, stateChanged, false }
-    <-stateChanged
-    close(stateChanged)
+	stateChanged := make(chan bool)
+	adder := func(p *factory) {
+		if subscriber != nil {
+			p.subscribers[t.name] = append(p.subscribers[t.name], subscriber)
+		}
+	}
+	t.p.stateModifier <- &stateModifierSpec{adder, stateChanged, false}
+	<-stateChanged
+	close(stateChanged)
 }
 
 func (t *tickerTopic) Close() error {
-    stateChanged := make(chan bool)
-    remover := func(state *factory) {
-        delete(state.topics, t.name)
-        delete(state.subscribers, t.name)
-        close(t.closeChannel)
-        t.ticker.Stop()
-    }
-    t.p.stateModifier <- &stateModifierSpec { remover, stateChanged, false }
-    <-stateChanged
-    close(stateChanged)
-    return nil
+	stateChanged := make(chan bool)
+	remover := func(state *factory) {
+		if _, exists := state.topics[t.name]; exists {
+			delete(state.topics, t.name)
+			delete(state.subscribers, t.name)
+			close(t.closeChannel)
+			t.ticker.Stop()
+		}
+	}
+	t.p.stateModifier <- &stateModifierSpec{remover, stateChanged, false}
+	<-stateChanged
+	close(stateChanged)
+	return nil
 }
 
 func runTicker(topic *tickerTopic, t *factory) <-chan bool {
-    releaser := make(chan bool)
-    go func() {
-        close(releaser)
-        for ;; {
-            select {
-            case <-topic.closeChannel:
-                return
-            case snapshot := <-topic.ticker.C:
-                go func() {
-                    t.events<- &eventSpec { topic.name, snapshot }
-                }()
-            }
-        }
-    }()
-    return releaser
+	releaser := make(chan bool)
+	go func() {
+		close(releaser)
+		for {
+			select {
+			case <-topic.closeChannel:
+				return
+			case snapshot := <-topic.ticker.C:
+				go func() {
+					t.events <- &eventSpec{topic.name, snapshot}
+				}()
+			}
+		}
+	}()
+	return releaser
 }
-
